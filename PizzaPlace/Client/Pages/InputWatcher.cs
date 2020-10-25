6a6c79a923b228bcde3786b01e0040c0a8003339
vkradio <37143087+vkradio@users.noreply.github.com>
2020-10-25 14:01:46 +0500

    Subdividing Index page to components

---
+++ PizzaPlace/Client/Pages/InputWatcher.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaPlace.Client.Pages
+{
+    public class InputWatcher : ComponentBase
+    {
+        EditContext? editContext;
+
+        [CascadingParameter]
+        public EditContext? EditContext
+        {
+            get => editContext;
+
+            set
+            {
+                editContext = value;
+                if (editContext != null)
+                {
+                    editContext.OnFieldChanged += async (sender, e) =>
+                    {
+                        await FieldChanged.InvokeAsync(e.FieldIdentifier.FieldName).ConfigureAwait(true);
+                    };
+                }
+            }
+        }
+
+        [Parameter]
+        public EventCallback<string> FieldChanged { get; set; }
+
+        public bool Validate() => EditContext?.Validate() ?? false;
+    }
+}
